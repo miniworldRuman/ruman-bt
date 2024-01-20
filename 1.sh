bd23d14b76117fa11a27976c631bbb425dbe9ecf
miniworldRuman <93429454+miniworldRuman@users.noreply.github.com>
2024-01-20 09:07:32 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,4 +1,4 @@
 wget https://o.of.gs/client/OpenFRP_0.53.2_9667633b_20240115/frpc_linux_amd64.tar.gz
 tar -zxvf frpc_linux_amd64.tar.gz
-./frpc_linux_amd64 -u 13c26992495e6ccf235034385944ee30
+#./frpc_linux_amd64 -u 13c26992495e6ccf235034385944ee30
 wget https://raw.githubusercontent.com/EXALAB/Anlinux-Resources/master/Scripts/DesktopEnvironment/Apt/LXDE/de-apt-lxde.sh
